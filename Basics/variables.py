032023322b2952ff15390520781f8e00133d17dc
Saurabh Agarwal <saurabhagarwal.official@gmail.com>
2021-10-07 00:06:22 +0530

    Variables

---
+++ Basics/variables.py
@@ -0,0 +1,9 @@
+character_name = "Tom" #string variable
+character_age = "50"
+is_male = True  #Boolean Variable
+print("once there was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+
+character_name = "Mike"
+print("He really liked his name " + character_name + ",")
+print("but didnt like being " + character_age + ".")
