0a0b8bcdca42f95a3714c2f0459c8167d3facad3
Saurabh Agarwal <saurabhagarwal.official@gmail.com>
2021-10-15 22:07:48 +0530

    For Loop

---
+++ Basics/for_loop.py
@@ -0,0 +1,21 @@
+#For loop on a string
+for letter in "Giraffe Academy":
+    print(letter)
+
+#For loop on an array
+friends = ["Jim", "Kelly", "John", "Tom"]
+for friend in friends:
+    print(friend)
+
+#For loop with a range
+for i in range(10):
+    print(i)
+
+#For loop with a specific range
+for r in range(3, 10):
+    print(r)
+
+#For loop with range of friends array and printing values from friends
+for index in range(len(friends)):
+    print(friends[index])
+
