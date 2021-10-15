96566ef215d36bb886417b6f69fb30dd09fbbdbd
Saurabh Agarwal <saurabhagarwal.official@gmail.com>
2021-10-15 18:31:25 +0530

    functions in python

---
+++ Basics/functions.py
@@ -0,0 +1,13 @@
+#To start a function first write 'def [name of function]():'
+#Code under function would execute only if correct indentation is followed.
+
+def say_hi():               #basic function
+    print("Hi User")
+
+say_hi()        #calling the function
+
+def say_name(name):         #parameterizing the function
+    print("Hello " + name)
+
+say_name("Saurabh")         #passing the value for name parameter
+
