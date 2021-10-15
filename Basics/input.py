262552ddc4ecf438b6fee4b017188375df603f44
Saurabh Agarwal <saurabhagarwal.official@gmail.com>
2021-10-15 22:27:11 +0530

    Repository restructure

--- Basics/input.py
+++
@@ -1,3 +0,0 @@
-name = input("Enter your name: ")   #inputting the value and storing it in a variable
-age = input("Enter your age: ")
-print("Hi " + name + "! you are " + age)                 #displaying the same value
