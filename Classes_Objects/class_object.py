6f02115e74f0f5d07beff6c48f271cd22418a622
Saurabh Agarwal <saurabhagarwal.official@gmail.com>
2021-10-17 00:59:56 +0530

    Classes and Objects Usage

---
+++ Classes_Objects/class_object.py
@@ -0,0 +1,6 @@
+from student import Student
+
+student1 = Student('Jim', 'Business', 3.1, False)       #student1 is an object of Student class
+student2 = Student('Pam', 'Art', 2.8, True)
+
+print(student2.gpa)
---
+++ Classes_Objects/student.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, name, major, gpa, is_on_probation):          #initialised function
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probabtion = is_on_probation
