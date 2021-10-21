4ddd127cd2f6b62eca87ade324712ad7bab820ba
Saurabh Agarwal <saurabhagarwal.official@gmail.com>
2021-10-21 18:45:54 +0530

    Multiple choice quiz

--- Multiple_Choice_Quiz/app.py
+++ Multiple_Choice_Quiz/app.py
@@ -19,7 +19,7 @@ def run_test(questions):
         answer = input(question.prompt)
         if answer == question.answer:
             score += 1
-    print("You got " + str(score) + '/' + str(len(questions)) + ' correct')
+    print("You got " + str(score) + '/' + str(len(questions)) + " correct")
 
 
 run_test(questions)
